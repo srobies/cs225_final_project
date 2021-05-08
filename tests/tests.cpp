5f54b868e9f92c5353ab0c22608347cb73bb2bfc
slr6 <slr6@illinois.edu>
2021-05-08 11:52:28 -0500

    Update tests to work with new Dijkstra's output

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -254,54 +254,54 @@ TEST_CASE("DFS test with full dataset") {
 TEST_CASE("Dijkstra's Algorithm Test 1") {
   AirportsData test = AirportsData();
   int GKA_index = test.getAirportIndex("GKA");
-  vector<vector<int>> dijkstras_goroka = test.dijkstras(GKA_index);
+  vector<pair<int,int>> dijkstras_goroka = test.dijkstras(GKA_index);
 
   // the shortest path between the source and itself is 0
-  REQUIRE(dijkstras_goroka[GKA_index][0] == 0);
+  REQUIRE(dijkstras_goroka[GKA_index].first == 0);
 
   // shortest path between GKA and LAE is 1 flight
   int test_lae = test.getAirportIndex("LAE");
-  REQUIRE(dijkstras_goroka[test_lae][0] == 1);
+  REQUIRE(dijkstras_goroka[test_lae].first == 1);
 
   // shortest path between GKA and POM is 1 flight
   int test_pom = test.getAirportIndex("POM");
-  REQUIRE(dijkstras_goroka[test_pom][0] == 1);
+  REQUIRE(dijkstras_goroka[test_pom].first == 1);
 }
 
 TEST_CASE("Dijkstra's Algorithm Test 2") {
   AirportsData test = AirportsData();
   int ohare_idx = test.getAirportIndex("ORD");
-  vector<vector<int>> dijkstras_ohare = test.dijkstras(ohare_idx);
+  vector<pair<int,int>> dijkstras_ohare = test.dijkstras(ohare_idx);
 
   // the shortest path between the source and itself is 0
-  REQUIRE(dijkstras_ohare[ohare_idx][0] == 0);
+  REQUIRE(dijkstras_ohare[ohare_idx].first == 0);
 
   // shortest path between ORD and ATL is 1 flight
   int test_atl = test.getAirportIndex("ATL");
-  REQUIRE(dijkstras_ohare[test_atl][0] == 1);
+  REQUIRE(dijkstras_ohare[test_atl].first == 1);
 
   // shortest path between ORD and JFK is 1 flight
   int test_jfk = test.getAirportIndex("JFK");
-  REQUIRE(dijkstras_ohare[test_jfk][0] == 1);
+  REQUIRE(dijkstras_ohare[test_jfk].first == 1);
 }
 
 TEST_CASE("Dijkstra's Algorithm Test 3") {
   AirportsData test = AirportsData();
   int champaign_idx = test.getAirportIndex("CMI");
-  vector<vector<int>> dijkstras_champaign = test.dijkstras(champaign_idx);
+  vector<pair<int,int>> dijkstras_champaign = test.dijkstras(champaign_idx);
 
   // the shortest path between the source and itself is 0
-  REQUIRE(dijkstras_champaign[champaign_idx][0] == 0);
+  REQUIRE(dijkstras_champaign[champaign_idx].first == 0);
 
   // shortest path between CMI and ORD is 1 flight
   int test_ord = test.getAirportIndex("ORD");
-  REQUIRE(dijkstras_champaign[test_ord][0] == 1);
+  REQUIRE(dijkstras_champaign[test_ord].first == 1);
 
   // shortest path between CMI and AZO is 2 flights
   int test_azo = test.getAirportIndex("AZO");
-  REQUIRE(dijkstras_champaign[test_azo][0] == 2);
+  REQUIRE(dijkstras_champaign[test_azo].first == 2);
 
   // shortest path between CMI and YYZ is 2 flights
   int test_yyz = test.getAirportIndex("YYZ");
-  REQUIRE(dijkstras_champaign[test_yyz][0] == 2);
+  REQUIRE(dijkstras_champaign[test_yyz].first == 2);
 }
